870a3a665fdacd0650aa15fc202b4ba51a3d8b4e
SvilenD <42715140+SvilenD@users.noreply.github.com>
2020-01-09 12:34:10 +0200

    updated

---
+++ Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace TetrisGame
+{
+    using System;
+
+    public static class Calculator
+    {
+        static readonly int ChangeLevel = 1000;
+
+        public static int GetLevel(int score)
+        {
+            return Math.Min(15, 1 + (score / ChangeLevel));
+        }
+
+        public static int FramesToMoveFigure(int level)
+        {
+            return 15 - level;
+        }
+    }
+}
